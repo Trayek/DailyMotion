814df51425891821a5029662d2e0f7e738064aca
Trayek <jzi@sitecore.net>
2021-03-23 12:25:43 +0000

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # DailyMotion
 Simple sample integration with DailyMotion
+
+## How it works
+open javascript\dailymotion.js and update the following:
+Line 8: Replace [username-or-id] with the username of the DailyMotion user. If necessary, update the fields that are returned as well as things like limits (no limit specified means 10 results will be returned).
+Line 17: Replace [path-to-your-item-here] with the path to the parent item holding all video items, replacing the '/' character with %2F and spaces with %20. 
+Line 22: Replace [your-template-id-here] with the ID of your template to base the video items on. 
+The fields specified in lines 20-25 are dependent on the field names in the template as well as the fields specified in the URL on line 7.
+
+**Important** 
+Make sure you specify your Content-Security-Policy as well by adding: 
+connect-src 'self' https://*.dailymotion.com/;
+
+(or similar). 
---
+++ javascript/DailyMotion.js
@@ -0,0 +1,36 @@
+﻿define(["sitecore"], function (Sitecore) {
+  Sitecore.Commands.DailyMotionSync =
+  {
+    canExecute: function (context) {
+      return true;
+    },
+    execute: function (context) {
+	  fetch("https://api.dailymotion.com/user/[username-or-id]/videos?fields=id%2Ctags%2Cdescription%2Cduration%2Ctitle")
+	  .then((resp) => resp.json())
+	  .then(function(data) {
+		let videos = data.list;
+		alert ("found " + videos.length + " videos to import" );
+		
+		var i;
+		for (i = 0; i < videos.length; i++) {
+			var xhr = new XMLHttpRequest();
+			  xhr.open("POST", origin + "/sitecore/api/ssc/item/[path-to-your-item-here]");
+			  xhr.setRequestHeader("Content-Type", "application/json");
+			  var obj = {};
+			  obj['ItemName'] = videos[i].id;
+			  obj['__Display Name'] = videos[i].title;
+			  obj['TemplateID'] = "[your-template-id-here]";
+			  obj['Title'] = videos[i].title;
+			  obj['DailyMotionMovie'] = videos[i].id;
+			  obj['Description'] = videos[i].description;
+			  // and more fields if needed
+			  xhr.send(JSON.stringify(obj, null, 4));
+		}
+	  })
+	  .catch(function(error) {
+		  alert ("something went wrong");
+		  console.log(error);
+	  });
+    }
+  };
+});
